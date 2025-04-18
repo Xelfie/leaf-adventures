e39db5fe29a7b94acc19e98836dcec0d16696606
Xelfie <d.marine99@yahoo.co.uk>
2025-04-19 01:45:07 +0400

    feat: add more animations on player

--- character/character.gd
+++ character/character.gd
@@ -14,11 +14,15 @@ func _physics_process(delta: float) -> void:
 	if input_direction != Vector2.ZERO:
 		input_direction = input_direction.normalized()
 		velocity = input_direction * SPEED
-		$AnimatedSprite2D.play("moving")
 		
-		# Flip character based on horizontal movement
-		if input_direction.x != 0:
+		if input_direction.x != 0 && input_direction.y == 0:
+			$AnimatedSprite2D.play("walking_left_right")
 			$AnimatedSprite2D.flip_h = input_direction.x > 0
+		
+		if input_direction.y != 0:
+			$AnimatedSprite2D.play("walking_up")
+			$AnimatedSprite2D.flip_v = input_direction.y > 0
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
